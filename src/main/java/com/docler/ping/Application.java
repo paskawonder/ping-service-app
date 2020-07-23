c5956cc914d64610c379bce2043fdebf62675e27
Pavel Polubentcev <pavel.polubentcev@mail.ru>
2020-07-23 14:58:11 +0200

    development

--- src/main/java/com/docler/ping/Application.java
+++ src/main/java/com/docler/ping/Application.java
@@ -1,9 +1,22 @@
 package com.docler.ping;
 
+import com.docler.ping.config.ApplicationConfig;
+import com.docler.ping.executor.GlobalExecutor;
+import com.docler.ping.model.Task;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
+
+import java.util.List;
+
 public final class Application {
 
     public static void main(final String[] args) {
-
+        final Injector injector = Guice.createInjector(new ApplicationConfig());
+        final List<Task> tasks = injector.getInstance(Key.get(new TypeLiteral<>() {}));
+        final GlobalExecutor globalExecutor = injector.getInstance(GlobalExecutor.class);
+        globalExecutor.execute(tasks);
     }
 
 }
